1e097498681d8abb2e35685a0c8333d0ce248d2d
unknown <raqueochoam@gmail.com>
2024-05-21 04:17:50 -0600

    run simulator in html

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, send_from_directory, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory
+import subprocess
 import os
 
 app = Flask(__name__)
 
-# Ruta para servir el archivo JSON
-@app.route('/daily_statistics.json')
-def daily_statistics():
-    return send_from_directory(os.path.join(app.root_path, ''), 'daily_statistics.json')
-
-# Ruta para servir la página principal
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/run-simulation', methods=['POST'])
+def run_simulation():
+    try:
+        result = subprocess.run(['python', 'python/Simulation.py'], capture_output=True, text=True)
+        if result.returncode == 0:
+            return jsonify({'success': True, 'output': result.stdout})
+        else:
+            return jsonify({'success': False, 'error': result.stderr})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+
+@app.route('/daily_statistics.json')
+def get_daily_statistics():
+    return send_from_directory(os.getcwd(), 'daily_statistics.json')
 
 if __name__ == '__main__':
     app.run(debug=True)
